95e8a38b9ed236ef10709d089cc73facc8bd6430
cjk1cob <Akksaya.RajasriGP@in.bosch.com>
2023-07-27 15:08:07 +0530

    Type Parametrize

--- test/typeParametrizationTest.cpp
+++ test/typeParametrizationTest.cpp
@@ -1,258 +1,47 @@
-[3:02 PM] Ismail Anootha (MS/ECP2-XC)
-
 #include "gtest/gtest.h"
-
-
-
-
 using testing::Types;
 
-
-
-
- 
-
-
-
-
 class ITempSensor{
-
-
-
-
     public:
-
-
-
-
     virtual int getOutSideTemp()=0;
-
-
-
-
 };
-
-
-
-
 class ModelATempSensor:public ITempSensor{
-
-
-
-
     public:
-
-
-
-
-    int getOutSideTemp(){
-
-
-
-
-        return 23;
-
-
-
-
-    }
-
-
-
-
+    int getOutSideTemp(){ return  23;}
 };
-
-
-
-
 class ModelBTempSensor:public ITempSensor{
-
-
-
-
     public:
-
-
-
-
-    int getOutSideTemp(){
-
-
-
-
-        return 23;
-
-
-
-
-    }
-
-
-
-
+    int getOutSideTemp(){ return  23;}
 };
-
-
-
-
 class AutoTempRegulator{
-
-
-
-
-    //DIP: Abstraction
-
-
-
-
     ITempSensor* tempSensorPtr;
-
-
-
-
     public:
-
-
-
-
-    AutoTempRegulator(ITempSensor* address):tempSensorPtr(address){}
-
-
-
-
-    int regulateTemp(){
-
-
-
-
-        return this->tempSensorPtr->getOutSideTemp();
-
-
-
-
-    }
-
-
-
-
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address} {}
+    void  regulateTemp(){
+         this->tempSensorPtr->getOutSideTemp();
+    }  
 };
-
-
-
-
- 
-
-
-
 
 template <typename T>
-
-
-
-
 ITempSensor* createObject();
-
-
-
-
 template <>
-
-
-
-
 ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
-
-
-
-
 template <>
-
-
-
-
 ITempSensor* createObject<ModelBTempSensor>() { return new ModelBTempSensor(); }
 
-
-
-
- 
-
-
-
-
 template <typename T>
-
-
-
-
 class TempSensorFixture:public testing::Test{
-
-
-
-
     protected:
-
-
-
-
-    //Arrange
-
-
-
-
+    TempSensorFixture():objUnderTest{ createObject<T>()}{}
+    //arrange
     ITempSensor* objUnderTest;
 
-
-
-
-    TempSensorFixture() :objUnderTest{ createObject<T>() } {}
-
-
-
-
 };
-
-
-
-
- 
-
-
-
 
 typedef Types<ModelATempSensor,ModelBTempSensor> Implementations;
 
-
-
-
- 
-
-
-
-
 TYPED_TEST_SUITE(TempSensorFixture, Implementations);
 
-
-
-
- 
-
-
-
-
-TYPED_TEST(TempSensorFixture, GetTempTest) {
-
-
-
-
+TYPED_TEST(TempSensorFixture,GetTempTest){
     ASSERT_EQ(this->objUnderTest->getOutSideTemp(),23);
-
-
-
-
 }
-
-
-
-
- 
