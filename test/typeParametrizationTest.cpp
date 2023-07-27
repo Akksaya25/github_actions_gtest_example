9d79f40ba5edd48e14f9a7f8a29128d67dab9225
cjk1cob <Akksaya.RajasriGP@in.bosch.com>
2023-07-27 14:52:03 +0530

    Fixture test with valParametrize

---
+++ test/typeParametrizationTest.cpp
@@ -0,0 +1,55 @@
+#include "gtest/gtest.h"
+using testing::Types;
+
+class ITempSensor{
+    public:
+    virtual int getOutsideTemp()=0;
+
+};
+class ModelATempSensor:public ITempSensor{
+    public:
+    int getOutsideTemp(){ return 23;}
+};
+class ModelBTempSensor:public ITempSensor{
+    public:
+    int getOutsideTemp(){ return 23;}
+};
+class AutoTempRegulator{
+    ITempSensor* tempSensorPtr;
+    public:
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address}{}
+    void regulateTemp(){
+        this->tempSensorPtr->getOutsideTemp();
+    }
+};
+template <typename T>
+ITempSensor* createObject();
+template <>
+ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
+template <>
+ITempSensor* createObject<ModelBTempSensor>() { return new ModelATempSensor(); }
+
+template <typename T>
+class TempSensorFixture:public testing::Test{
+    protected:
+    TempSensorFixture():objUnderTest{createObject<T>()}
+    //arrange
+    ITempSensor* objUnderTest;
+
+}
+typedef Types<ModelATempSensor,ModelBTempSensor> Implementations;
+
+TYPED_TEST_SUITE(TempSensorFixture, Implementations);
+
+TEST_TEST(TempSensorFixture,GetTempTest){
+    ASSERT_EQ(objUnderTest.getOutsideTemp(),23)
+}
+// class ModelBTempSensorFixture:public testing::Test{
+//     protected:
+//     //arrange
+//     ModelBTempSensor objUnderTest;
+
+// }
+// TEST_F(ModelBTempSensorFixture,GetTempTest){
+//     ASSERT_EQ(objUnderTest.getOutsideTemp(),23)
+// }
