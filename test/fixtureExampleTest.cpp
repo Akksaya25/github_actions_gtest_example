2230e21a4a8df740284bad76813e3aa152b3b6c9
cjk1cob <Akksaya.RajasriGP@in.bosch.com>
2023-07-27 11:46:47 +0530

    Fixture test

--- test/fixtureExampleTest.cpp
+++ test/fixtureExampleTest.cpp
@@ -42,37 +42,62 @@ class A{
 };
 
 
-
-
-TEST(TestFixtureTestSuite,OperationTrueTest){
-
-    //Arrange
-
-    C cObj;
-
-    B bObj(&cObj);
-
-    A obj(&bObj);
-
-    //Act and Assert
-
-    ASSERT_TRUE(obj.operation());
-
+class TestFixtureTestSuite:public testing: :Test{
+    protected:
+    A* aPtr;
+    B* bPtr;
+    C* cPtr;
+    TestFixtureTestSuite(){
+        //initialization
+    }
+    TestFixtureTestSuite(){
+        //Releasing the Resource
+    }
+    void SetUp(){
+        //Arrange
+        this->cPtr-new C();
+        this->bPtr-new B(cPtr);
+        this->aPtr-new A(bPtr);
+    }
+    void TearDown(){
+        //delete resource
+        delete cPtr;
+        delete bPtr;
+        delete aPtr;
+    }
 }
+
+
+
+ TEST(TestFixtureTestSuite,OperationTrueTest){
+
+//     //Arrange
+
+//     C cObj;
+
+//     B bObj(&cObj);
+
+//     A obj(&bObj);
+
+//     //Act and Assert
+
+     ASSERT_TRUE(aPtr->operation());
+
+ }
 
 TEST(TestFixtureTestSuite,OperationFalseTest){
 
     //Arrange
 
-    C cObj;
+    //C cObj;
 
-    B bObj(&cObj);
+    //B bObj(&cObj);
 
-    A obj(&bObj);
+    //A obj(&bObj);
 
     //Act and Assert
 
-    ASSERT_FALSE(obj.operation());
+    ASSERT_FALSE(aPtr->operation());
 
 
 
