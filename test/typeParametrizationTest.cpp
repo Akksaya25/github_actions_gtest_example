122f8aa6dd9774728c162e7e3b4a002be17dfe86
cjk1cob <Akksaya.RajasriGP@in.bosch.com>
2023-07-27 15:03:46 +0530

    Type Parametrize

--- test/typeParametrizationTest.cpp
+++ test/typeParametrizationTest.cpp
@@ -3,53 +3,45 @@ using testing::Types;
 
 class ITempSensor{
     public:
-    virtual int getOutsideTemp()=0;
-
+    virtual int getOutSideTemp()=0;
 };
 class ModelATempSensor:public ITempSensor{
     public:
-    int getOutsideTemp(){ return 23;}
+    int getOutSideTemp(){ return  23;}
 };
 class ModelBTempSensor:public ITempSensor{
     public:
-    int getOutsideTemp(){ return 23;}
+    int getOutSideTemp(){ return  23;}
 };
 class AutoTempRegulator{
     ITempSensor* tempSensorPtr;
     public:
-    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address}{}
-    void regulateTemp(){
-        this->tempSensorPtr->getOutsideTemp();
-    }
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address} {}
+    void  regulateTemp(){
+         this->tempSensorPtr->getOutSideTemp();
+    }  
 };
+
 template <typename T>
 ITempSensor* createObject();
-
+template <>
 ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
-
-ITempSensor* createObject<ModelBTempSensor>() { return new ModelATempSensor(); }
+template <>
+ITempSensor* createObject<ModelBTempSensor>() { return new ModelBTempSensor(); }
 
 template <typename T>
 class TempSensorFixture:public testing::Test{
     protected:
-    TempSensorFixture():objUnderTest{createObject<T>()}{}
+    TempSensorFixture():objUnderTest{ createObject<T>()}{}
     //arrange
     ITempSensor* objUnderTest;
 
 };
+
 typedef Types<ModelATempSensor,ModelBTempSensor> Implementations;
 
 TYPED_TEST_SUITE(TempSensorFixture, Implementations);
 
-TEST_TEST(TempSensorFixture,GetTempTest){
-    ASSERT_EQ(this->objUnderTest->getOutsideTemp(),23)
+TYPED_TEST(TempSensorFixture,GetTempTest){
+    ASSERT_EQ(this->objUnderTest->getOutSideTemp(),23);
 }
-// class ModelBTempSensorFixture:public testing::Test{
-//     protected:
-//     //arrange
-//     ModelBTempSensor objUnderTest;
-
-// }
-// TEST_F(ModelBTempSensorFixture,GetTempTest){
-//     ASSERT_EQ(objUnderTest.getOutsideTemp(),23)
-// }
